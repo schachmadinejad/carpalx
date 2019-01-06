909fac4a25ffb3df446f6a20c72d62de4a9d0b4e
Tobias Hahn <morowinger@gmail.com>
2019-01-06 23:39:12 +0100

    added corrected programmatic files, added images

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -223,7 +223,7 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_Backspace, Key_Spacebar, Key_LeftGui, Key_LeftControl,
+   Key_Backspace, Key_Spacebar, ShiftToLayer(FUNCTION), Key_LeftControl,
    Key_CapsLock,
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
@@ -257,17 +257,17 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
+  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           ___,
    Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
    ___, Key_Delete, ___, ___,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   ___, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
+   ___,    ___, Consumer_ScanPreviousTrack,     Consumer_ScanNextTrack,    ___, ___, Key_F12,
+                               Key_PcApplication,          Key_LeftArrow,            Key_DownArrow,              Key_UpArrow,  Key_RightArrow,              ___,
+   ___,          ___,          Consumer_PlaySlashPause, Consumer_Mute, Consumer_VolumeDecrement,             Consumer_VolumeIncrement,    ___,
    ___, ___, Key_Enter, ___,
    ___)
 ) // KEYMAPS(
